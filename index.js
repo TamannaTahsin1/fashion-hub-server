ec41e7693365c6231bc3e0d9e60ba3c3f972a010
TamannaTahsin1 <tamannatahsin648@gmail.com>
2023-10-17 20:36:55 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors')
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+// for root
+app.get('/',(req, res) => {
+    res.send('Fashion server is running')
+})
+app.listen(port, () =>{
+    console.log(`Fashion server is running on port: ${port}`)
+})
