8b4dca3b486a517378de3e27f7f2d2527797278f
TamannaTahsin1 <tamannatahsin648@gmail.com>
2023-10-19 19:03:13 +0600

    create and read data for cart

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-const express = require('express');
-const cors = require('cors')
-require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+/** @format */
+
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
-
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.u5hejig.mongodb.net/?retryWrites=true&w=majority`;
-
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -20,7 +19,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -28,36 +27,58 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // to insert data collection
-    const productsCollection = client.db('productsDB').collection('products')
-    const userCollection = client.db('productsDB').collection('user')
-
+    const productsCollection = client.db("productsDB").collection("products");
+    const userCollection = client.db("productsDB").collection("user");
+    const cartCollection = client.db("productsDB").collection("cart");
     // ******PRODUCT RELATED APIs********
     // read the created data
-    app.get('/products', async(req, res) =>{
-        const cursor = productsCollection.find();
-        const result = await cursor.toArray();
-        res.send(result)
-    })
+    app.get("/products", async (req, res) => {
+      const cursor = productsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
     // create data of client side
-    app.post('/products', async(req, res) => {
-        const newProducts = req.body
-        console.log(newProducts);
-        const result = await productsCollection.insertOne(newProducts)
-        res.send(result)
-    })
+    app.post("/products", async (req, res) => {
+      const newProducts = req.body;
+      console.log(newProducts);
+      const result = await productsCollection.insertOne(newProducts);
+      res.send(result);
+    });
+    // *******Cart related APIs**********
+    // read the created data
+    app.get("/cart", async (req, res) => {
+      const cursor = cartCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    // create data 
+    app.post("/cart", async (req, res) => {
+      const cartProduct = req.body;
+      console.log(cartProduct);
+      const result = await cartCollection.insertOne(cartProduct);
+      res.send(result);
+    });
 
     // *******USER RELATED APIs***********
+    // read the created data
+    app.get("/user", async (req, res) => {
+      const cursor = productsCollection.find();
+      const user = await cursor.toArray();
+      res.send(user);
+    });
     // create data of user
-    app.post('/user', async(req, res) => {
-      const user = req.body
+    app.post("/user", async (req, res) => {
+      const user = req.body;
       console.log(user);
-      const result = await userCollection.insertOne(user)
-      res.send(result)
-  })
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -65,11 +86,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
 // for root
-app.get('/',(req, res) => {
-    res.send('Fashion server is running')
-})
-app.listen(port, () =>{
-    console.log(`Fashion server is running on port: ${port}`)
-})
+app.get("/", (req, res) => {
+  res.send("Fashion server is running");
+});
+app.listen(port, () => {
+  console.log(`Fashion server is running on port: ${port}`);
+});
